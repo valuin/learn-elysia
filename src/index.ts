79fd38b8f68c07cff341652515d2435bf2baf105
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2025-01-10 21:41:59 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
+
+const app = new Elysia()
+  .use(swagger())
+  .get('/', () => 'Hello Elysia') 
+  .get("/test", "Test Route")
+  .post("/hello", "Do you miss me?")
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
